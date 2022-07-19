0a4e427f476db034fc8b3882c390c35b4b308b3b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-07-19 17:41:37 +0600

    feat: created models for  PrizeBondDraw and DrawWinner

--- prizebond_draw_checker/prize_bond_draw_manager/models.py
+++ prizebond_draw_checker/prize_bond_draw_manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from dustu_lib.models import TimeStampedUUIDModel
 
-# Create your models here.
+
+class PrizeBondDraw(TimeStampedUUIDModel):
+    draw_term = models.IntegerField(blank=True, null=True)
+    draw_date = models.DateField(blank=True, null=True)
+
+
+class DrawWinner(TimeStampedUUIDModel):
+    winning_number = models.CharField(max_length=7, null=False, blank=False)
+    prize_bracket = models.CharField(max_length=6, null=False, blank=False)
+
+    draw = models.ForeignKey(
+        PrizeBondDraw, related_name="draw_winners", on_delete=models.CASCADE
+    )
