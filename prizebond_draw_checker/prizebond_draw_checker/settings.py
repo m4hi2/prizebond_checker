936ef09ce2465456e444cfb4b96f0e0ec06d315a
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-07-19 17:41:54 +0600

    feat: installed prize_bond_draw_manager app in setting

--- prizebond_draw_checker/prizebond_draw_checker/settings.py
+++ prizebond_draw_checker/prizebond_draw_checker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "prize_bond_draw_manager.apps.PrizeBondDrawManagerConfig",
 ]
 
 MIDDLEWARE = [
