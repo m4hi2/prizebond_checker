5dd31f9f247a004e0d7d62e6d011c5ce7dd3d63b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-07-28 17:01:09 +0600

    feat: templates dir added

--- prizebond_draw_checker/prizebond_draw_checker/settings.py
+++ prizebond_draw_checker/prizebond_draw_checker/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "prizebond_draw_checker.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
