d574794724bcfa9350343ddd3989eebf3f6c8867
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-08-06 13:48:49 +0600

    change: access_type to offline for getting tokens
    Google doesn't provide access token for online type.
    https://django-allauth.readthedocs.io/en/latest/providers.html#django-configuration

--- prizebond_draw_checker/prizebond_draw_checker/settings.py
+++ prizebond_draw_checker/prizebond_draw_checker/settings.py
@@ -162,7 +162,7 @@ SOCIALACCOUNT_PROVIDERS = {
             "email",
         ],
         "AUTH_PARAMS": {
-            "access_type": "online",
+            "access_type": "offline",
         },
     }
 }
