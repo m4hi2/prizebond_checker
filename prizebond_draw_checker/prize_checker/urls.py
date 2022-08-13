f548cf4bf55deec1c423e4bfaed573139aafd392
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-08-13 18:45:26 +0600

    feat: set the dummy profile page as homepage for the time

--- prizebond_draw_checker/prize_checker/urls.py
+++ prizebond_draw_checker/prize_checker/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile", views.profile),
+    path("", views.profile),
     path("add", views.add_prize_bond_number, name="add")
 ]
